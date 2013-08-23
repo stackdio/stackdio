d90e7f813c8486aa8c95289fefddabffd5baa4ad
Steve Brownlee <chortlehoort@gmail.com>
2013-08-23 11:19:03 -0500

    PI-54 - New stack now being pushed in the Stacks store rather than trying to force a new API call to load

--- stackdio/core/static/js/app/api/Stacks.js
+++ stackdio/core/static/js/app/api/Stacks.js
@@ -1,63 +1,63 @@
 define(["lib/q", "app/stores", "app/models"], function (Q, stores, models) {
-        return {
-            load : function () {
-                var deferred = Q.defer();
+    return {
+        load : function () {
+            var deferred = Q.defer();
 
-                $.ajax({
-                    url: '/api/stacks/',
-                    type: 'GET',
-                    headers: {
-                        "X-CSRFToken": stackdio.csrftoken,
-                        "Accept": "application/json"
-                    },
-                    success: function (response) {
-                        var i, item, items = response.results;
-                        var stack;
+            $.ajax({
+                url: '/api/stacks/',
+                type: 'GET',
+                headers: {
+                    "X-CSRFToken": stackdio.csrftoken,
+                    "Accept": "application/json"
+                },
+                success: function (response) {
+                    var i, item, items = response.results;
+                    var stack;
 
 
-                        // Clear the store and the grid
-                        stores.Stacks.removeAll();
+                    // Clear the store and the grid
+                    stores.Stacks.removeAll();
 
-                        for (i in items) {
-                            stack = new models.Stack().create(items[i]);
+                    for (i in items) {
+                        stack = new models.Stack().create(items[i]);
 
-                            // Inject the record into the store
-                            stores.Stacks.push(stack);
-                        }
-
-                        console.log('stacks', stores.Stacks());
-
-                        // Resolve the promise and pass back the loaded stacks
-                        deferred.resolve(stores.Stacks());
-
+                        // Inject the record into the store
+                        stores.Stacks.push(stack);
                     }
-                });
 
-                return deferred.promise;
-            },
-            save: function (stack) {
-                var deferred = Q.defer();
+                    console.log('stacks', stores.Stacks());
 
-                stack.hosts = JSON.stringify(stack.hosts);
+                    // Resolve the promise and pass back the loaded stacks
+                    deferred.resolve(stores.Stacks());
 
-                $.ajax({
-                    url: '/api/stacks/',
-                    type: 'POST',
-                    data: stack,
-                    headers: {
-                        "X-CSRFToken": stackdio.csrftoken,
-                        "Accept": "application/json"
-                    },
-                    success: function (response) {
-                        self.load();
-                        deferred.resolve();
-                    }
-                });
+                }
+            });
 
-                return deferred.promise;
-            },
-            delete: function (record) {
+            return deferred.promise;
+        },
+        save: function (stack) {
+            var deferred = Q.defer();
 
-            }
+            stack.hosts = JSON.stringify(stack.hosts);
+
+            $.ajax({
+                url: '/api/stacks/',
+                type: 'POST',
+                data: stack,
+                headers: {
+                    "X-CSRFToken": stackdio.csrftoken,
+                    "Accept": "application/json"
+                },
+                success: function (response) {
+                    stores.Stacks.push(response);
+                    deferred.resolve();
+                }
+            });
+
+            return deferred.promise;
+        },
+        delete: function (record) {
+
         }
+    }
 });
