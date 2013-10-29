892508c4815596bdd6316b16ad2c2649dfeb5f1f
Abe Music <abe.music@gmail.com>
2013-10-29 20:35:54 +0000

    PI-173: Implement the file_hash method of fileserver backend...seems to be the missing piece of serving up "regular" files from salt master

--- stackdsalt/extmods/fileserver/stackdio.py
+++ stackdsalt/extmods/fileserver/stackdio.py
@@ -141,7 +141,50 @@ def update():
     log.warn('stackdio fileserver - udpate called')
 
 def file_hash(load, fnd):
-    log.warn('stackdio fileserver - file_hash called')
+    '''
+    Return a file hash, the hash type is set in the master config file
+    '''
+    if 'path' not in load or 'env' not in load:
+        return ''
+    path = fnd['path']
+    ret = {}
+
+    # if the file doesn't exist, we can't get a hash
+    if not path or not os.path.isfile(path):
+        log.warn('Path does not exist or is not a file: {0}'.format(path))
+        return ret
+
+    # set the hash_type as it is determined by config-- so mechanism won't change that
+    ret['hash_type'] = __opts__['hash_type']
+
+    # check if the hash is cached
+    # cache file's contents should be "hash:mtime"
+    cache_path = os.path.join(__opts__['cachedir'],
+                              'roots/hash',
+                              load['env'],
+                              '{0}.hash.{1}'.format(fnd['rel'],
+                              __opts__['hash_type']))
+
+    # if we have a cache, serve that if the mtime hasn't changed
+    if os.path.exists(cache_path):
+        with salt.utils.fopen(cache_path, 'rb') as fp_:
+            hsum, mtime = fp_.read().split(':')
+            if os.path.getmtime(path) == mtime:
+                # check if mtime changed
+                ret['hsum'] = hsum
+                return ret
+
+    # if we don't have a cache entry-- lets make one
+    ret['hsum'] = salt.utils.get_hash(path, __opts__['hash_type'])
+    cache_dir = os.path.dirname(cache_path)
+    # make cache directory if it doesn't exist
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    # save the cache object "hash:mtime"
+    with salt.utils.fopen(cache_path, 'w') as fp_:
+        fp_.write('{0}:{1}'.format(ret['hsum'], os.path.getmtime(path)))
+
+    return ret
 
 def file_list_emptydirs(load):
     log.warn('stackdio fileserver - file_list_emptydirs called')
