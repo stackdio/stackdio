774a57478d93663968c0ed0e9407785f618b6eae
Abe Music <abe.music@gmail.com>
2014-03-03 17:39:39 +0000

    PI-248: Apply indenting to only requests that specify application/json accept headers

--- stackdio/core/middleware.py
+++ stackdio/core/middleware.py
@@ -1,5 +1,6 @@
 class JSONIndentAcceptHeaderMiddleware(object):
 
     def process_request(self, request):
-        request.META['HTTP_ACCEPT'] = 'application/json; indent=4'
+        if request.META.get('HTTP_ACCEPT') == 'application/json':
+            request.META['HTTP_ACCEPT'] = 'application/json; indent=4'
         return None
