776c388d5559846ccc29605afb0710243037d608
Clark Perkins <clark.perkins@digitalreasoning.com>
2014-07-10 22:54:18 +0000

    Added docstrings to permissions

--- stackdio/core/permissions.py
+++ stackdio/core/permissions.py
@@ -2,14 +2,19 @@ from rest_framework import permissions
 
 
 class AdminOrOwnerPermission(permissions.IsAdminUser):
-
+    """
+    A permission that allows access to owners and admins only
+    """
     def has_object_permission(self, request, view, obj):
         return request.user == obj.owner \
                or super(AdminOrOwnerPermission, self) \
                     .has_object_permission(request, view, obj)
 
 class AdminOrOwnerOrPublicPermission(AdminOrOwnerPermission):
-
+    """
+    A permission that allows safe methods through for public objects and
+    all access to owners and admins
+    """
     def has_object_permission(self, request, view, obj):
         if super(AdminOrOwnerOrPublicPermission, self) \
                 .has_object_permission(request, view, obj):
