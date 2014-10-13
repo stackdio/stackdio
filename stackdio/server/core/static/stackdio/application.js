73f18cd1346a3ac316157086ce2d48478a8f36cf
cory.hughes <cory.hughes@digitalreasoning.com>
2014-10-13 14:55:11 -0500

    Added underscore resolution, added ladda path to application.js

--- stackdio/server/core/static/stackdio/application.js
+++ stackdio/server/core/static/stackdio/application.js
@@ -1,18 +1,18 @@
 /*!
   * Copyright 2014,  Digital Reasoning
-  * 
+  *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
   * You may obtain a copy of the License at
-  * 
+  *
   *     http://www.apache.org/licenses/LICENSE-2.0
-  * 
+  *
   * Unless required by applicable law or agreed to in writing, software
   * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
-  * 
+  *
 */
 
 requirejs.config({
@@ -21,7 +21,8 @@ requirejs.config({
         'bootstrap': 'bower_components/bootstrap/dist/js/bootstrap.min',
         'bootstrap-select': 'bower_components/bootstrap-select/bootstrap-select.min',
         'bootstrap-typeahead': 'bower_components/typeahead.js/dist/typeahead.min',
-        'bootbox': 'bower_components/bootbox/bootbox',
+        'bootbox': 'bower_components/bootbox/',
+        'ladda': 'bower_components/ladda/js/ladda',
         'knockout': 'bower_components/knockout.js/knockout',
         'q': 'bower_components/q/q.min',
         'underscore': 'bower_components/underscore/underscore',
@@ -42,7 +43,7 @@ require([
     'bootstrap-typeahead',
     'underscore',
     'api/Root'
-], 
+],
 function (select, typeahead, _, RootAPI, navigation) {
 
     // Set up a template compiler for underscore
