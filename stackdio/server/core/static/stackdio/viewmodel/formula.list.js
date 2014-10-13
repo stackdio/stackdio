a53877267fadc964882adccc5314c868cef447db
cory.hughes <cory.hughes@digitalreasoning.com>
2014-10-13 14:31:54 -0500

    Moved references to ladda

--- stackdio/server/core/static/stackdio/viewmodel/formula.list.js
+++ stackdio/server/core/static/stackdio/viewmodel/formula.list.js
@@ -1,27 +1,27 @@
 /*!
   * Copyright 2014,  Digital Reasoning
-  * 
+  *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
   * You may obtain a copy of the License at
-  * 
+  *
   *     http://www.apache.org/licenses/LICENSE-2.0
-  * 
+  *
   * Unless required by applicable law or agreed to in writing, software
   * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
   * limitations under the License.
-  * 
+  *
 */
 
 define([
-    'q', 
+    'q',
     'knockout',
     'bootbox',
     'util/galaxy',
     'util/alerts',
-    'util/ladda',
+    'ladda',
     'util/formula',
     'store/Formulas',
     'api/api'
@@ -52,7 +52,7 @@ function (Q, ko, bootbox, $galaxy, alerts, Ladda, formulautils, FormulaStore, AP
         try {
             $galaxy.join(self);
         } catch (ex) {
-            console.log(ex);            
+            console.log(ex);
         }
 
         /*
