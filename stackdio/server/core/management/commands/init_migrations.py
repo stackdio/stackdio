003a7c5cb545de4519478b2920c1a0ab2ffb963f
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-04-30 14:35:28 -0500

    Fixed references to /api since it always redirects to /api/

--- stackdio/server/core/management/commands/init_migrations.py
+++ stackdio/server/core/management/commands/init_migrations.py
@@ -27,6 +27,7 @@ from django.core.management.base import BaseCommand, CommandError
 import logging
 logger = logging.getLogger('core')
 
+
 class Command(BaseCommand):
     args = 'app-name, app-name, ...'
     help = 'Deletes all migrations for the given app and re-initializes them.'
@@ -61,7 +62,6 @@ class Command(BaseCommand):
             logger.info('Executing manage.py schemamigration {0} ' \
                         '--initial'.format(app_name))
             call_command('schemamigration', app_name, initial=True)
-            
 
     def error(self, msg=''):
         logger.error(msg)
