2eb1b12ae277c6d684bcaa83d278e9f39e6a8ada
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-05-13 21:00:41 -0500

    PI-450 fixed root api view after it was broken on the last commit

--- stackdio/server/api_v1/urls.py
+++ stackdio/server/api_v1/urls.py
@@ -18,97 +18,100 @@
 
 from django.conf.urls import patterns, include, url
 
-from rest_framework.decorators import api_view
+from rest_framework.compat import OrderedDict
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.views import APIView
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
+class APIRootView(APIView):
     """
     Root of the stackd.io API. Below are all of the API endpoints that
     are currently accessible. Each API will have its own documentation
     and particular parameters that may discoverable by browsing directly
     to them.
-
     """
-    api = {
-        'core': {
-            'settings': reverse('usersettings-detail',
-                                request=request,
-                                format=format),
-            'change_password': reverse('change_password',
-                                       request=request,
-                                       format=format),
-            'version': reverse('version',
-                               request=request,
-                               format=format),
-        },
-        'cloud': {
-            'provider_types': reverse('cloudprovidertype-list',
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        api = OrderedDict((
+            ('core', {
+                'settings': reverse('usersettings-detail',
+                                    request=request,
+                                    format=format),
+                'change_password': reverse('change_password',
+                                           request=request,
+                                           format=format),
+                'version': reverse('version',
+                                   request=request,
+                                   format=format),
+            }),
+            ('cloud', OrderedDict((
+                ('provider_types', reverse('cloudprovidertype-list',
+                                           request=request,
+                                           format=format)),
+                ('providers', reverse('cloudprovider-list',
                                       request=request,
-                                      format=format),
-            'providers': reverse('cloudprovider-list',
-                                 request=request,
-                                 format=format),
-            'instance_sizes': reverse('cloudinstancesize-list',
+                                      format=format)),
+                ('profiles', reverse('cloudprofile-list',
+                                     request=request,
+                                     format=format)),
+                ('instance_sizes', reverse('cloudinstancesize-list',
+                                           request=request,
+                                           format=format)),
+                ('regions', reverse('cloudregion-list',
+                                    request=request,
+                                    format=format)),
+                ('zones', reverse('cloudzone-list',
+                                  request=request,
+                                  format=format)),
+                ('snapshots', reverse('snapshot-list',
                                       request=request,
-                                      format=format),
-            'profiles': reverse('cloudprofile-list',
-                                request=request,
-                                format=format),
-            'regions': reverse('cloudregion-list',
-                               request=request,
-                               format=format),
-            'zones': reverse('cloudzone-list',
-                             request=request,
-                             format=format),
-            'snapshots': reverse('snapshot-list',
+                                      format=format)),
+                ('security_groups', reverse('securitygroup-list',
+                                            request=request,
+                                            format=format)),
+            ))),
+            ('blueprints', reverse('blueprint-list',
+                                   request=request,
+                                   format=format)),
+            ('formulas', reverse('formula-list',
                                  request=request,
-                                 format=format),
-            'security_groups': reverse('securitygroup-list',
-                                       request=request,
-                                       format=format),
-        },
-        'hosts': reverse('host-list',
-                         request=request,
-                         format=format),
-        'snapshots': reverse('snapshot-list',
-                             request=request,
-                             format=format),
-        'stacks': reverse('stack-list',
-                          request=request,
-                          format=format),
-        'volumes': reverse('volume-list',
-                           request=request,
-                           format=format),
-        'blueprints': reverse('blueprint-list',
+                                 format=format)),
+            ('stacks', reverse('stack-list',
+                               request=request,
+                               format=format)),
+            ('hosts', reverse('host-list',
                               request=request,
-                              format=format),
-        'formulas': reverse('formula-list',
-                            request=request,
-                            format=format),
-        'search': reverse('search',
-                          request=request,
-                          format=format),
-    }
+                              format=format)),
+            ('snapshots', reverse('snapshot-list',
+                                  request=request,
+                                  format=format)),
+            ('volumes', reverse('volume-list',
+                                request=request,
+                                format=format)),
+            ('search', reverse('search',
+                               request=request,
+                               format=format)),
+        ))
 
-    if request.user.is_staff:
-        api['core']['users'] = reverse('user-list',
-                                       request=request,
-                                       format=format)
-        api['global_orchestration_formulas'] = reverse('formula-global-orchestration-list',
-                                                       request=request,
-                                                       format=format)
+        if request.user.is_staff:
+            api['core']['users'] = reverse('user-list',
+                                           request=request,
+                                           format=format)
+            api['global_orchestration_formulas'] = reverse('formula-global-orchestration-list',
+                                                           request=request,
+                                                           format=format)
 
-    return Response(api)
+        return Response(api)
 
 
 urlpatterns = patterns(
     '',
 
-    url(r'^$', api_root),
+    url(r'^$', APIRootView.as_view()),
 
     ##
     # IMPORTS URLS FROM ALL APPS
