334c342d825a6c875366b288024b11739b8d1371
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-05-26 17:25:03 -0500

    Removed unnecessary config from celery file

--- stackdio/stackdio/celery.py
+++ stackdio/stackdio/celery.py
@@ -1,13 +1,7 @@
 from __future__ import absolute_import
 
-import os
-
 from celery import Celery
-
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stackdio.settings.production')
-
-from django.conf import settings  # NOQA
+from django.conf import settings
 
 app = Celery('stackdio')
 
