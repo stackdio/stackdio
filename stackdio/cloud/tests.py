9c5292bff91ce9d93ef5e3ce38caaea94d08c347
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-05-26 12:38:39 -0500

    Wrote tests that pass

--- stackdio/cloud/tests.py
+++ stackdio/cloud/tests.py
@@ -23,9 +23,10 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 import logging
-import sys
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APIClient
 
 
@@ -36,13 +37,31 @@ class CloudProviderTypeTestCase(TestCase):
 
     def setUp(self):
         self.client = APIClient()
+        user_model = get_user_model()
+        user_model.objects.create_superuser('test.admin', 'test.admin@digitalreasoning.com', '1234')
+        user_model.objects.create_user('test.user', 'test.user@digitalreasoning.com', '1234')
+        self.client.login(username='test.admin', password='1234')
 
     def test_authenticated(self):
+        # Ensure we are logged out before we try the first request
+        self.client.logout()
         response = self.client.get('/api/provider_types/')
-        self.assertEqual(response.status_code, 402)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Non - admins shouldn't be able to see this endpoint
+        self.client.login(username='test.user', password='1234')
+        response = self.client.get('/api/provider_types/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_provider_types(self):
+        # Admins SHOULD be able to see this endpoint
+        response = self.client.get('/api/provider_types/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_provider_type(self):
         response = self.client.post('/api/provider_types/', {'title': 'new'})
+        logger.debug(response.data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class CloudProviderTestCase(TestCase):
