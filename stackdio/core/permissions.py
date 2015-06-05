b7ed032f637a2b69b6f9bdf7626c07f6a56cc4c4
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-06-05 10:45:50 -0500

    PI-451 removed permission classes having to do with owners

--- stackdio/core/permissions.py
+++ stackdio/core/permissions.py
@@ -115,38 +115,6 @@ class StackdioParentObjectPermissions(StackdioObjectPermissions):
         return True
 
 
-class AdminOrOwnerPermission(permissions.IsAdminUser):
-    """
-    A permission that allows access to owners and admins only
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner \
-            or super(AdminOrOwnerPermission, self).has_permission(request, view)
-
-    # Override this so as not to use the one from permissions.IsAdminUser
-    def has_permission(self, request, view):
-        return True
-
-
-class AdminOrOwnerOrPublicPermission(AdminOrOwnerPermission):
-    """
-    A permission that allows safe methods through for public objects and
-    all access to owners and admins
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Give all permission to owners and admins
-        if super(AdminOrOwnerOrPublicPermission, self).has_object_permission(request, view, obj):
-            return True
-
-        # Give read-only access to public objects
-        if request.method in permissions.SAFE_METHODS:
-            return obj.public
-        else:
-            return False
-
-
 class IsAdminOrReadOnly(permissions.IsAdminUser):
     """
     A permission that allows all users read-only permission and admin users
