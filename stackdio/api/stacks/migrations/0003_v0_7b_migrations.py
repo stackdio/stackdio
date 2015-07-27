2bdbf684051dbfdeece2da321f125ba3a2028f95
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-07-27 18:04:10 -0500

    PI-466 migrations for stack refacotr

---
+++ stackdio/api/stacks/migrations/0003_v0_7b_migrations.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('stacks', '0002_v0_7_migrations'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='stack',
+            name='namespace',
+            field=models.CharField(max_length=64, verbose_name=b'Namespace'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='stack',
+            unique_together=set([('title',)]),
+        ),
+    ]
