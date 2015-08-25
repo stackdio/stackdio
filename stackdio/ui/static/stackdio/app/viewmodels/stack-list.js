d127f05bc1965f7f420fdb4fabea8f17397e5f90
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-08-25 18:53:14 -0500

    PI-485 no longer make items in the list observable

--- stackdio/ui/static/stackdio/app/viewmodels/stack-list.js
+++ stackdio/ui/static/stackdio/app/viewmodels/stack-list.js
@@ -126,7 +126,7 @@ define([
                         stackModel.availableActions(self.actionMap[rawStack.id]);
                     }
 
-                    stackModels.push(ko.observable(stackModel));
+                    stackModels.push(stackModel);
                 });
                 self.stacks(stackModels);
 
@@ -147,8 +147,8 @@ define([
 
                     // Find the current stack with the correct ID, and load the actions
                     for (var i = 0, length = stacks.length; i < length; ++i) {
-                        if (stacks[i]().id === id) {
-                            stacks[i]().loadAvailableActions();
+                        if (stacks[i].id === id) {
+                            stacks[i].loadAvailableActions();
                             break;
                         }
                     }
