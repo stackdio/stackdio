96f0d3d156c77cbe7d778e1b35c904ea8b386e95
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-08-26 17:51:07 -0500

    PI-459 added label model & migration

---
+++ stackdio/core/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Label',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(max_length=255, verbose_name=b'Key')),
+                ('value', models.CharField(max_length=255, verbose_name=b'Value')),
+                ('object_id', models.PositiveIntegerField()),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+        ),
+    ]
