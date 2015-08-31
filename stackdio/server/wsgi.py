0321bf32ea8522966f6e8e53571c212b6621ad15
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-08-31 11:14:07 -0500

    Have the circle build create a wheel distribution also

--- stackdio/server/wsgi.py
+++ stackdio/server/wsgi.py
@@ -41,7 +41,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stackdio.server.settings.produc
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application  # NOQA
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
