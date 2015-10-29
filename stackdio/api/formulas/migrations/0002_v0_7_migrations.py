da204f7f945e5c1fcdc598593336e3296ed3b4c9
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-10-29 14:45:11 -0500

    Fixed formula migration issues

--- stackdio/api/formulas/migrations/0002_v0_7_migrations.py
+++ stackdio/api/formulas/migrations/0002_v0_7_migrations.py
@@ -1,7 +1,57 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 
+import os
+
+from django.conf import settings
 from django.db import models, migrations
+
+from stackdio.api.formulas.tasks import import_formula, FormulaTaskException
+import shutil
+
+
+def forward(apps, schema_editor):
+    FormulaComponent = apps.get_model('formulas', 'FormulaComponent')
+    Formula = apps.get_model('formulas', 'Formula')
+
+    formulas = {}
+
+    for formula in Formula.objects.all():
+        if formula.uri not in formulas:
+            formulas[formula.uri] = formula
+            continue
+
+        # Otherwise we need to delete the formula and everything associated with it
+        for component in FormulaComponent.objects.filter(formula=formula):
+            for bhfc in component.blueprinthostformulacomponent_set.all():
+                try:
+                    bhfc.component = FormulaComponent.objects.get(sls_path=bhfc.component.sls_path,
+                                                                  formula=formulas[formula.uri])
+                    bhfc.save()
+                except FormulaComponent.DoesNotExist:
+                    bhfc.component.formula = formulas[formula.uri]
+                    bhfc.component.save()
+
+            component.delete()
+
+        formula.delete()
+
+    # re-import all the formulas
+    for formula in Formula.objects.all():
+        # there's nothing we can do about private repos without having the password :(
+        if not formula.git_username:
+            try:
+                import_formula(formula.id, '')
+            except FormulaTaskException as e:
+                if 'SPECFILE' in e.message:
+                    print('Skipping import of formula: {0}'.format(formula.uri))
+                else:
+                    raise
+
+    # remove the old ones
+    old_formula_dir = os.path.join(settings.STACKDIO_CONFIG['storage_root'], 'user_states')
+    if os.path.isdir(old_formula_dir):
+        shutil.rmtree(old_formula_dir)
 
 
 class Migration(migrations.Migration):
@@ -12,6 +62,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(forward),
         migrations.AlterModelOptions(
             name='formula',
             options={'ordering': ['pk'], 'default_permissions': ('admin', 'create', 'delete', 'update', 'view')},
