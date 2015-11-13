6ed3fab57c142307dcd6a0e706e6e88ff19ef8a1
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-11-13 15:45:05 -0600

    Cleaned up build script, updated docs

--- packer/build.sh
+++ packer/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Grab the base dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${DIR}" )" && pwd )"
+
+cd ${DIR}
+
+# install bower dependencies
+bower install
+
+# Build the UI
+python manage.py build_ui
+
+# Grab the version
 export STACKDIO_VERSION=`python setup.py --version`
 
+# Create our artifact
 python setup.py bdist_wheel
 
+# Run the build
 packer build packer/build.json
