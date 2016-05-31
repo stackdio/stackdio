2ec51c3fe3e35a1c3edaf51d0932b2cfbab461a5
Clark Perkins <clark.perkins@digitalreasoning.com>
2016-05-30 22:51:39 -0500

    Removed extra import

--- stackdio/ui/management/commands/build_ui.py
+++ stackdio/ui/management/commands/build_ui.py
@@ -22,7 +22,6 @@ import sys
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.template import Context
 from django.template.loader import get_template
 
 STATIC_DIR = os.path.join(
