69bb9865afd88e1b767d9c5183776361a9309ffa
Clark Perkins <clark.perkins@digitalreasoning.com>
2016-09-22 17:39:17 -0500

    Accidentally removed docstring from api root

--- stackdio/api/urls.py
+++ stackdio/api/urls.py
@@ -31,6 +31,12 @@ from rest_framework.views import APIView
 
 
 class APIRootView(APIView):
+    """
+    Root of the stackd.io API. Below are all of the API endpoints that
+    are currently accessible. Each API will have its own documentation
+    and particular parameters that may discoverable by browsing directly
+    to them.
+    """
 
     def get(self, request, format=None):
         api = OrderedDict((
