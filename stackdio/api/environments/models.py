d97475063b61b09f9bbad7e9ca9c6bc4628a6639
Clark Perkins <clark.perkins@digitalreasoning.com>
2016-12-08 10:30:04 -0600

    Added component status model

--- stackdio/api/environments/models.py
+++ stackdio/api/environments/models.py
@@ -19,9 +19,11 @@ from __future__ import unicode_literals
 
 import logging
 
+import six
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
+from stackdio.core.constants import ComponentStatus, Health
 from stackdio.core.fields import JSONField
 
 logger = logging.getLogger(__name__)
@@ -40,6 +42,7 @@ _environment_object_permissions = (
 )
 
 
+@six.python_2_unicode_compatible
 class Environment(TimeStampedModel):
 
     model_permissions = _environment_model_permissions
@@ -59,3 +62,74 @@ class Environment(TimeStampedModel):
 
     # The properties for this blueprint
     properties = JSONField('Properties')
+
+    def __str__(self):
+        return six.text_type('Environment {}'.format(self.name))
+
+
+class ComponentMetadataQuerySet(models.QuerySet):
+
+    def create(self, **kwargs):
+        if 'health' not in kwargs:
+            current_health = kwargs.pop('current_health', None)
+            if 'status' in kwargs:
+                kwargs['health'] = ComponentMetadata.HEALTH_MAP[kwargs['status']] \
+                                   or current_health \
+                                   or Health.UNKNOWN
+        return super(ComponentMetadataQuerySet, self).create(**kwargs)
+
+
+@six.python_2_unicode_compatible
+class ComponentMetadata(TimeStampedModel):
+
+    # Limited health map - since we don't know what the components are,
+    # we can't know if they're queued / running.
+    HEALTH_MAP = {
+        ComponentStatus.SUCCEEDED: Health.HEALTHY,
+        ComponentStatus.FAILED: Health.UNHEALTHY,
+        ComponentStatus.CANCELLED: None,
+    }
+
+    STATUS_CHOICES = tuple((x, x) for x in set(HEALTH_MAP.keys()))
+    HEALTH_CHOICES = tuple((x, x) for x in set(HEALTH_MAP.values()) if x is not None)
+
+    # Fields
+    sls_path = models.CharField('SLS Path', max_length=128)
+
+    host = models.CharField('Host', max_length=256)
+
+    environment = models.ForeignKey('Environment', related_name='component_metadatas')
+
+    status = models.CharField('Status',
+                              max_length=32,
+                              choices=STATUS_CHOICES,
+                              default=ComponentStatus.QUEUED)
+
+    health = models.CharField('Health',
+                              max_length=32,
+                              choices=HEALTH_CHOICES,
+                              default=Health.UNKNOWN)
+
+    objects = ComponentMetadataQuerySet.as_manager()
+
+    def __str__(self):
+        return six.text_type('Component {} for environment {} - {} ({})'.format(
+            self.sls_path,
+            self.environment.name,
+            self.status,
+            self.health,
+        ))
+
+    def set_status(self, status):
+        # Make sure it's a valid status
+        assert status in self.HEALTH_MAP
+
+        self.status = status
+
+        # Set the health based on the new status
+        new_health = self.HEALTH_MAP[status]
+
+        if new_health is not None:
+            self.health = new_health
+
+        self.save()
