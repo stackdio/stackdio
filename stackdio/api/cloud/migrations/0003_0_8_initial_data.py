504c3c5db73f9906fb8fa8b5f774e1d78d57c84a
Clark Perkins <clark.perkins@digitalreasoning.com>
2017-01-10 10:59:51 -0600

    Moved cloud object data migration into a post-migrate signal

--- stackdio/api/cloud/migrations/0003_0_8_initial_data.py
+++ stackdio/api/cloud/migrations/0003_0_8_initial_data.py
@@ -1,57 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals
 
-import json
-import os
-
 from django.db import migrations
 
 
-def load_initial_data():
-    cloud_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    with open(os.path.join(cloud_dir, 'fixtures', 'initial_data.json')) as f:
-        initial_data = json.load(f)
-
-    if initial_data is None:
-        print('Unable to load initial_data')
-
-    return initial_data
-
-
-def load_cloud_objects(apps, schema_editor):
-    initial_data = load_initial_data()
-
-    model_filter = {
-        'CloudProvider': 'name',
-        'CloudRegion': 'title',
-        'CloudZone': 'title',
-        'CloudInstanceSize': 'instance_id',
-    }
-
-    db_alias = schema_editor.connection.alias
-
-    for model in initial_data:
-        model_cls = apps.get_model('cloud', model['model'])
-        to_create = []
-        filter_attr = model_filter[model['model']]
-        for object_data in model['objects']:
-            # Only create if it's not already there
-            filtered = model_cls.objects.filter(**{filter_attr: object_data[filter_attr]})
-            if filtered.count() == 0:
-                to_create.append(model_cls(**object_data))
-            else:
-                # Update the fields to match the new stuff if the object already exists
-                for obj in filtered:
-                    for attr, val in object_data.items():
-                        setattr(obj, attr, val)
-                    obj.save()
-
-        model_cls.objects.using(db_alias).bulk_create(to_create)
-
-
 class Migration(migrations.Migration):
-
+    """
+    This migration is now a no-op.  The logic was moved into a post-migrate signal.
+    """
     initial = True
 
     dependencies = [
@@ -63,5 +19,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(load_cloud_objects),
+        migrations.RunPython(lambda a, s: None, lambda a, s: None),
     ]
