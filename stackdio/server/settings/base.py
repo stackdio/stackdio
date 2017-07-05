45f5b6f5ebc0c8d4ac6ea884d76eed7c04294004
Clark Perkins <clark.perkins@digitalreasoning.com>
2017-07-05 14:08:27 -0500

    remove acks_late

--- stackdio/server/settings/base.py
+++ stackdio/server/settings/base.py
@@ -400,9 +400,6 @@ CELERY_DEFAULT_QUEUE = 'default'
 # there are other idle workers
 CELERYD_PREFETCH_MULTIPLIER = 1
 
-# Also enable late acks, so tasks don't get acked until they're finished
-CELERY_ACKS_LATE = True
-
 # Serializer settings
 # We'll use json since pickle can sometimes be insecure
 CELERY_RESULT_SERIALIZER = 'json'
